80e49be724e3f57806396c7cabb1be4cc37c061d
Lorox10 <Lorenzo.vargass@comunidad.iush.edu.co>
2024-09-05 12:24:25 -0500

    se agregó el contenedor del equipo, se diseñó la apgina de Inicio y se hizo Responsive a todo el Inicio

--- Controllers/InicioController.py
+++ Controllers/InicioController.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route("/inicio")
 def Index():
     return render_template('Index.html')
+
+@app.route("/modoEdicion")
+def EditarCrear():
+    return render_template('_EditarCrear.html')
+
+@app.route("/modoVisualizacion")
+def Detalle():
+    return render_template('_Detalle.html')
