1d35b21ff78d4c4d393f131efbd0296969d68cbf
Alejandra Bedoya <piola2003@outlook.com>
2024-11-04 22:10:57 -0500

    Se reestructura la lógica de editar y actualizar. Ya funcionan las acciones y la visualización

--- Controllers/InicioController.py
+++ Controllers/InicioController.py
@@ -9,32 +9,32 @@ from Models.ServicioBd import AgregarSeccion, ObtenerSecciones, ObtenerSeccion, 
 def Index():
     return render_template('Index.html')
 
+
 @app.route("/modoEdicion")
 def DetalleEdicion():
+
     secciones = ObtenerSecciones()
-    if secciones.count == 0:
-        return render_template('_EditarCrear.html', idSeccion=0)
-    else:
-        return render_template('_DetalleEdicion.html', secciones=secciones, idSeccion=0)
-    
+    return render_template('_DetalleEdicion.html', secciones=secciones, idSeccion=0)
+
+
 @app.route("/Editar/<int:idSeccion>")
 def EditarCrear(idSeccion):
-    if idSeccion == 0:
-        seccion_data = None
-    else:
-        seccion_data = ObtenerSeccion(idSeccion)
-    
-    return render_template('_EditarCrear.html', idSeccion=idSeccion, seccion_data=seccion_data)
+    seccion = None
+    if idSeccion != 0:
+        seccion = ObtenerSeccion(idSeccion)
+    return render_template('_EditarCrear.html', idSeccion=idSeccion, seccion=seccion)
+
 
 @app.route("/modoVisualizacion")
 def Detalle():
     secciones = ObtenerSecciones()
-    return render_template('Visualizacion.html', secciones = secciones)
+    return render_template('_Detalle.html', secciones=secciones)
+
 
 @app.route('/ActualizarSeccion/<int:idSeccion>', methods=['POST'])
 def ActualizarSeccion(idSeccion):
-    nombre = request.form.get('nombre', '')  
-    contenido = request.form.get('contenido', '')  
+    nombre = request.form.get('nombre', '')
+    contenido = request.form.get('contenido', '')
 
     if idSeccion == 0:
         AgregarSeccion(nombre, contenido)
@@ -42,36 +42,11 @@ def ActualizarSeccion(idSeccion):
         ModificarSeccion(idSeccion, nombre, contenido)
 
     secciones = ObtenerSecciones()
-    return render_template('_DetalleEdicion.html', secciones=secciones, message="Registro modificado correctamente.")
+    return redirect(url_for('DetalleEdicion')) 
 
-@app.route('/EliminarSeccion/<section_id>', methods=['DELETE'])
+
+@app.route('/EliminarSeccion/<int:idSeccion>', methods=['POST'])
 def EliminarSeccion(idSeccion):
     EliminarSeccion(idSeccion)
     secciones = ObtenerSecciones()
-    return render_template('_EditarCrear.html', secciones = secciones)
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    try:
-        if 'upload' not in request.files:
-
-            return jsonify({'error': 'No file part'}), 400
-        
-        file = request.files['upload']
-        
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'}), 400
-        
-        if file:
-            # Genera un hash para el nombre del archivo
-            hash_filename = hashlib.md5(file.filename.encode()).hexdigest()[:10]
-            result = cloudinary.uploader.upload(file, public_id=hash_filename)
-            
-            return jsonify({
-                'url': result['secure_url'],
-                'uploaded': 1,
-                'fileName': result['original_filename']
-            })
-    
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    return redirect(url_for('DetalleEdicion'))
